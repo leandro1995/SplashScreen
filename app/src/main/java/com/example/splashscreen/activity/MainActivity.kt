b839e7c7d261fc26033c050429b5d3a23d6bb9e9
leandro1995 <leccbo1995@gmail.com>
2025-04-19 14:53:19 -0500

    Mostrar pantalla de ingreso a la app

--- app/src/main/java/com/example/splashscreen/activity/MainActivity.kt
+++ app/src/main/java/com/example/splashscreen/activity/MainActivity.kt
@@ -4,44 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.splashscreen.activity.ui.theme.SplashScreenTheme
+import com.example.splashscreen.R
+import com.example.splashscreen.ui.theme.SP_20
+import com.example.splashscreen.ui.theme.SplashScreenTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SplashScreenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ViewInit()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    private fun ViewInit() {
+        Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+            Text(
+                text = stringResource(R.string.welcome_text),
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontSize = SP_20
+            )
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SplashScreenTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        SplashScreenTheme {
+            ViewInit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/splashscreen/ui/theme/Dimen.kt
@@ -0,0 +1,5 @@
+package com.example.splashscreen.ui.theme
+
+import androidx.compose.ui.unit.sp
+
+val SP_20 = 20.sp
