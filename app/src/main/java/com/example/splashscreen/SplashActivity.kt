bc5bd17d9bcca734ea26b30f50bfaeed141847c0
LEANDRO\leccb <Leandro>
2025-04-19 03:09:36 -0500

    Modificar y arreglar ventana de splash para libreria

---
+++ app/src/main/java/com/example/splashscreen/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.splashscreen
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
